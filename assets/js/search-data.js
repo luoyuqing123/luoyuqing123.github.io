e7a1945b09242323267942b75101022a79925fbc
luoyuqing123 <p22314125@stu.ahu.edu.cn>
2026-03-24 13:30:09 +0000

    Deploying to gh-pages from @ luoyuqing123/luoyuqing123.github.io@4090b85a6ecda2572b68e9e11caba119c029784e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-个人简历",
+          title: "个人简历",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
