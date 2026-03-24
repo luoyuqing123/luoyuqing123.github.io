102cdc7b782b64ad2d364e6a01bcd3955d84e197
luoyuqing123 <p22314125@stu.ahu.edu.cn>
2026-03-24 12:03:55 +0000

    Deploying to gh-pages from @ luoyuqing123/luoyuqing123.github.io@588d040acfde5ff19343952c5a46765fabf4d8ab 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "luoyuqing123/-my-homepage",
+    "data-repo": "luoyuqing123/luoyuqing123.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
